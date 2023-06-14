d7703cc4b0a4cad36c524b187b64f25733c24ee1
CathyVisagie <134547360+CathyVisagie@users.noreply.github.com>
2023-06-14 21:44:41 +0200

    task1

---
+++ myVariables.js
@@ -0,0 +1,14 @@
+let myFirstNumber = 21
+let mySecondNumber = 62
+let myFirstString = "Hello my name is Cathy"
+let mySecondString = "I am pleased to meet you"
+let myBoolean = false
+let myMultiply = myFirstNumber * mySecondNumber
+let myFullString = "myFirstString, mySecondString"
+console.log (`The boolean is ${myBoolean}
+The first number is ${myFirstNumber}
+The second number is ${mySecondNumber}
+${myFirstNumber} X ${mySecondNumber} = ${myMultiply}
+The first string is ${myFirstString}
+The second string is ${mySecondString}
+These two together make: ${myFirstString} ${mySecondString}`)
